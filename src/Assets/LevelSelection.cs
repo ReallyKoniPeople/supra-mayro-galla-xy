9b946fda0bf6ebc7558fd17f159d7454a46313a1
TimArnoldd <arnold.tim2005@gmail.com>
2023-03-30 20:09:38 +0200

    Add Goomba

--- src/Assets/LevelSelection.cs
+++ src/Assets/LevelSelection.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void SelectLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
