4daa5da3027e9049a8c69243f6aa4234fe8c3822
D1i2m3 <arnold.tim2005@gmail.com>
2023-11-13 17:19:47 +0100

    Add MeshSimplifier script

---
+++ src/Assets/MeshSimplifier.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script can be used to reduce the amount of polygons for 3D-Models.
+/// </summary>
+public class MeshSimplifier : MonoBehaviour
+{
+    /// <summary>
+    /// A factor that the total amount of polygons gets multiplied.
+    /// Factor has to be lower for objects like spheres and higher for objects like cubes.
+    /// </summary>
+    public float quality = 0.01f;
+
+    void Start()
+    {
+        var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+        var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+        meshSimplifier.Initialize(originalMesh);
+        meshSimplifier.SimplifyMesh(quality);
+        var destMesh = meshSimplifier.ToMesh();
+        GetComponent<MeshFilter>().sharedMesh = destMesh;
+    }
+}
