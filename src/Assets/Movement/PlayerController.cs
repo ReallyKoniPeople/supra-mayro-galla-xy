25867ac9085d26e25b546832d510ed7300ec06a2
MarcelSteiner <marcelsteiner@bluewin.ch>
2023-11-16 21:00:36 +0100

    Fix Unlimited Jump Bug

--- src/Assets/Movement/PlayerController.cs
+++ src/Assets/Movement/PlayerController.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private float moveSpeed = 10f, rotateSpeed= 10f;
+    [SerializeField] private float moveSpeed = 10f, rotateSpeed = 10f;
     [SerializeField] private Transform playerModel;
     [SerializeField] private CustomGravity _customGravity;
     [SerializeField] private float jumpForce;
 
     private Vector3 playerInput;
-    private bool jump;
-    
+    private bool readyToJump;
+
     private void CheckInput()
     {
         playerInput.x = Input.GetAxisRaw("Horizontal");
         playerInput.z = Input.GetAxisRaw("Vertical");
-        if(!jump)
-        jump = Input.GetKeyDown(KeyCode.Space);
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump)
+        {
+            readyToJump = false;
+            Jump();
+        }
     }
 
     private void Update()
@@ -30,25 +34,26 @@ public class PlayerController : MonoBehaviour
     {
         RotateCharacter();
         Move();
-        if (jump)
-        {
-            jump = false;
-            Jump();
-        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if player collided with Object containing the "whatIsGround"-Layer
+        readyToJump = true;
     }
 
     private void Move()
     {
-        transform.position += playerModel.forward *playerInput.z* moveSpeed * Time.deltaTime;
+        transform.position += playerModel.forward * playerInput.z * moveSpeed * Time.deltaTime;
     }
 
     private void RotateCharacter()
     {
-        playerModel.Rotate(Vector3.up,playerInput.x*rotateSpeed*Time.deltaTime);
+        playerModel.Rotate(Vector3.up, playerInput.x * rotateSpeed * Time.deltaTime);
     }
 
     private void Jump()
     {
-        rb.AddForce(_customGravity.currentNormal*jumpForce,ForceMode.VelocityChange);
+        rb.AddForce(_customGravity.currentNormal * jumpForce, ForceMode.VelocityChange);
     }
 }
